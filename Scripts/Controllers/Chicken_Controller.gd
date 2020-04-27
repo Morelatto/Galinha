53f34a5681b3b676fa8b971459b574de5bbb8c84
Morel <morelatto@pm.me>
2020-04-27 01:13:03 -0300

    Mapped tileset and level.

--- Scripts/Controllers/Chicken_Controller.gd
+++ Scripts/Controllers/Chicken_Controller.gd
@@ -11,10 +11,10 @@ func apply_force(direction):
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _integrate_forces(state):
-	var xform = state.get_transform()
-	if xform.origin.x > screen_size.x:
-		xform.origin.x = 0
-	if xform.origin.x < 0:
-		xform.origin.x = screen_size.x
-	state.set_transform(xform)
+#func _integrate_forces(state):
+#	var xform = state.get_transform()
+#	if xform.origin.x > screen_size.x:
+#		xform.origin.x = 0
+#	if xform.origin.x < 0:
+#		xform.origin.x = screen_size.x
+#	state.set_transform(xform)
