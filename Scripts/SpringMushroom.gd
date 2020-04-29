50d89d63e303c73de88ed48089f5e7e2ab856f12
Joao <joaoschimmelpfeng@gmail.com>
2020-04-30 00:26:57 +0100

    Levels + shroom

--- Scripts/SpringMushroom.gd
+++ Scripts/SpringMushroom.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name SpringMushroom
 
 export var bounciness = 100
+export var direction = Vector2(0,10)
 
 func get_force(nPosition):
-	return (Vector2(0,-10) * bounciness)
+	return (direction * bounciness)
