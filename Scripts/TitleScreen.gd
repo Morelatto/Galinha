c13dde5474ac9382536cf47208dc2d55b1c6225a
Morel <morelatto@pm.me>
2020-04-29 00:45:47 -0300

    Added new title scene using animation player.

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,6 +1,9 @@
-extends Buttons
+extends Control
 
 export (PackedScene) var world
+
+func _ready():
+	$AnimationPlayer.play("StoryScroll")
 
 func _on_Play_pressed():
 	get_tree().change_scene_to(world)
