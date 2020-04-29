e613f32ba2f1cda718ea1a44cd31a1f31b29d53f
Joao <joaoschimmelpfeng@gmail.com>
2020-04-29 19:39:07 +0100

    Level loader

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -2,17 +2,21 @@ extends Position2D
 
 class_name Checkpoint
 # oh god why
-onready var chicken = get_parent().get_parent().get_parent().find_node("Chicken")
+onready var world_controller = get_tree().get_root().get_child(0)
+onready var chicken = get_tree().get_root().get_child(0).find_node("Chicken")
 onready var collisionShape = $StaticBody2D/CollisionShape2D
+onready var map = get_parent().get_parent()
 
 var is_triggered = false
 
 func _process(delta):
 	#32 is the chicken size
-	if !is_triggered and chicken.position.y < position.y - 32:
+	var local_chicken = map.to_local(chicken.position)
+	if !is_triggered and local_chicken.y < position.y - 32:
 		trigger()
-
+#-252.389
 func trigger():
+	world_controller.load_next_world()
 	collisionShape.disabled = false
 	chicken.checkpoint = self
 	is_triggered = true
---
+++ Scripts/Controllers/WorldController.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var world_list = []
+var loaded_worlds = []
+
+onready var chicken = find_node("Chicken")
+
+export var number_of_worlds = 3
+var world_index = 0
+
+func _ready():
+	load_world_list()
+	load_next_world()
+	load_next_world()
+	pass
+	
+func load_next_world():
+	var world = world_list[world_index].instance()
+	world_index += 1
+	if loaded_worlds.size() >= 1:
+		var last_world = loaded_worlds[loaded_worlds.size() - 1]
+		var bottom_of_map = world.get_node("LevelBottom")
+		world.position.y = last_world.position.y - bottom_of_map.position.y
+	else:
+		world.position.y = -world.get_node("LevelBottom").position.y
+	
+	loaded_worlds.push_back(world)
+	add_child(world)
+	
+	if loaded_worlds.size() > 3:
+		var deleteWorld = loaded_worlds[0]
+		remove_child(deleteWorld)
+		
+		loaded_worlds.pop_front()
+		call_deferred("free")
+		
+
+
+func load_world_list():
+	for i in range(number_of_worlds):
+		var world = load("res://Scenes/Stages/World_" + str(i) + ".tscn")
+		world_list.append(world)
