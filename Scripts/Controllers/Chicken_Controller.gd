d3439f9e88c93f83dcd2dc5b2b17ae6b60881a62
Joao <joaoschimmelpfeng@gmail.com>
2020-04-30 11:34:15 +0100

    Grey chicken

--- Scripts/Controllers/Chicken_Controller.gd
+++ Scripts/Controllers/Chicken_Controller.gd
@@ -16,6 +16,13 @@ onready var sound = $AudioStreamPlayer2D
 var can_kick = true
 var is_falling = false
 
+var counting_delay = false
+var delay_for_kick = 0.2
+var delay_for_kick_count = 0
+
+var unavailable_color = Color(0.8,0.8,0.8)
+var available_color = Color(1,1,1)
+
 var is_in_respawn = false
 
 func _ready():
@@ -43,7 +50,7 @@ func respawn():
 	is_in_respawn = false
 
 func _process(delta):
-	can_kick = linear_velocity.length() < MIN_KICK_VELOCITY
+	check_if_can_kick(delta)	
 	is_falling = linear_velocity.y > FALLING_VELOCITY_THRESHOLD
 	var state = "Idle"
 	if is_falling:
@@ -53,6 +60,24 @@ func _process(delta):
 	if animation.get_animation() != state:
 		animation.set_animation(state)
 
+func check_if_can_kick(delta):
+	if linear_velocity.length() < MIN_KICK_VELOCITY:
+		counting_delay = true
+		#can_kick = true
+	else:
+		counting_delay = false
+		delay_for_kick_count = 0
+		can_kick = false
+		$AnimatedSprite.modulate = unavailable_color
+	
+	if counting_delay:
+		delay_for_kick_count += delta
+		if delay_for_kick_count > delay_for_kick:
+			can_kick = true
+			counting_delay = false
+			delay_for_kick_count = 0
+			$AnimatedSprite.modulate = available_color
+		
 
 func _on_Chicken_body_entered(body):
 	if body.get_parent() is Spike:
